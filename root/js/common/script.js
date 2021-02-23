93898c035d11b79acde0424966c9e99fd434a511
中村隼 <nakamurashun@SHUN.local>
2021-02-23 20:40:53 +0900

    “msg”

--- root/js/common/script.js
+++ root/js/common/script.js
@@ -79,4 +79,31 @@ $(function ()
     }
 
 
+    /* fadeIn
+-------------------------------------------------------------------- */
+    $(window).on('load scroll', function ()
+    {
+
+        var box = $('.fadeIn');
+        var animated = 'animated';
+
+        box.each(function ()
+        {
+
+            var boxOffset = $(this).offset().top;
+            var scrollPos = $(window).scrollTop();
+            var wh = $(window).height();
+
+            //画面内のどの位置で処理を実行するかで「100」の値を変更
+            if (scrollPos > boxOffset - wh + 50)
+            {
+                $(this).addClass(animated);
+            }
+        });
+
+    });
+
+
 });
+
+
